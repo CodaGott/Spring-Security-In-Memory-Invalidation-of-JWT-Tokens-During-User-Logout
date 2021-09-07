3ca153f87752a8b70bc68b40a0fce997af1a5edf
Uyanwune Chidozie Chukwuebuka <talk2don90@gmail.com>
2021-09-07 23:55:11 +0100

    update

---
+++ src/main/java/com/example/springbootsecurity/model/RefreshToken.java
@@ -0,0 +1,36 @@
+package com.example.springbootsecurity.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+import java.time.Instant;
+
+import static javax.persistence.GenerationType.SEQUENCE;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class RefreshToken {
+
+    @Id
+    @GeneratedValue(strategy = SEQUENCE, generator = "refresh_token_seq")
+    @SequenceGenerator(name = "refresh_token_seq", allocationSize = 1)
+    private Long id;
+
+    @Column(name = "TOKEN" , nullable = false, unique = true)
+    private String token;
+
+    @OneToOne(optional = false, cascade = CascadeType.ALL)
+    @JoinColumn(name = "USER_DEVICE_ID", unique = true)
+    private UserDevice userDevice;
+
+    @Column(name = "REFRESH_COUNT")
+    private Long refreshCount;
+
+    @Column(name = "EXPIRY_DT", nullable = false)
+    private Instant expiryDate;
+}
