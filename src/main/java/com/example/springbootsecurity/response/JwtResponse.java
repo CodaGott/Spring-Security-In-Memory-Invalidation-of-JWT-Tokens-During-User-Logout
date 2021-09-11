7ac60f09fc994fa567de260019802ec3de8483d0
Uyanwune Chidozie Chukwuebuka <talk2don90@gmail.com>
2021-09-11 21:04:52 +0100

    update

---
+++ src/main/java/com/example/springbootsecurity/response/JwtResponse.java
@@ -0,0 +1,27 @@
+package com.example.springbootsecurity.response;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class JwtResponse {
+
+
+    private String accessToken;
+
+    private String refreshToken;
+
+    private String tokenType;
+
+    private Long expiryDuration;
+
+    public JwtResponse(String accessToken, String refreshToken, Long expiryDuration) {
+        this.accessToken = accessToken;
+        this.refreshToken = refreshToken;
+        this.expiryDuration = expiryDuration;
+        tokenType = "Bearer ";
+    }
+}
