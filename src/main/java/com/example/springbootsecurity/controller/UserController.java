6fbfed064cc0013f5b003a88e5a7ab707061be61
Uyanwune Chidozie Chukwuebuka <talk2don90@gmail.com>
2021-09-12 19:19:17 +0100

    get user profile by id  controller completed

--- src/main/java/com/example/springbootsecurity/controller/UserController.java
+++ src/main/java/com/example/springbootsecurity/controller/UserController.java
@@ -11,10 +11,7 @@ import com.example.springbootsecurity.service.UserPrincipal;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -59,6 +56,15 @@ public class UserController {
             }
         }
         return userProfiles;
+    }
+
+    @GetMapping("/byId/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
+    public UserProfile getUserProfileById(@PathVariable Long id){
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("User", "id", id));
+
+        return new UserProfile(user.getId(), user.getEmail(), user.getName(), user.getActive());
     }
 
 }
